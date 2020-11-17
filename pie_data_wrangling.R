14efed97244fbff3ebbb321154f44ba6b4b3789f
j-verstaen <julietteverstaen@ucsb.edu>
2020-11-17 15:25:06 -0800

    incorporated what survey data we currently have

--- pie_data_wrangling.R
+++ pie_data_wrangling.R
@@ -3,17 +3,18 @@
 ## Author: Juliette Verstaen
 ## Date: 11/14/2020
 
-
 library(tidyverse) 
 library(here)
+library(tm)
 
+'%!in%' <- function(x,y)!('%in%'(x,y))
+
+## FiveThirtyEight data
 thanksgiving_raw <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2018/2018-11-20/thanksgiving_meals.csv')
 
 
 ## There are a lot of naming mismatches unfortunately, but we can fix that! Note: Will group together all cheesecake flavors into one, and custards as well.
-
-
-thanksgiving_pie <- thanksgiving_raw %>% 
+fivethirtyeight_pies <- thanksgiving_raw %>% 
   dplyr::select(pie1:pie11, pie13) %>% 
   pivot_longer(cols =  pie1:pie13, names_to = "pies_num") %>% 
   select(type = value) %>% 
@@ -52,16 +53,43 @@ thanksgiving_pie <- thanksgiving_raw %>%
   dplyr::summarise(Count = sum(Count)) %>% 
   ungroup() %>% 
   rename(Flavor = Type) %>% 
-  mutate(Type = case_when(Flavor %in% c("buttermilk", "chocolate", "mincemeat", "other", "sweet potato") ~ "other",
+  mutate(Type = case_when(Flavor %in% c("buttermilk", "chocolate", "mincemeat", "other", "sweet potato", "pecan") ~ "other",
                           Flavor == "none" ~ "none", 
-                          T ~ "fruit"),
-         r_ladies_count = Count) %>% 
-  select(flavor = Flavor, type = Type, survey_counts = Count, r_ladies_count) %>% 
-  mutate(survery_sum = sum(survey_counts),
-         r_sum = sum(r_ladies_count)) %>% 
+                          T ~ "fruit")) %>% 
+  select(flavor = Flavor, type = Type, survey_counts = Count) %>% 
+  mutate(survery_sum = sum(survey_counts)) %>% 
   rowwise() %>% 
-  mutate(survey_percent = survey_counts/survery_sum,
-         r_percent = r_ladies_count/r_sum)  %>% 
-  select(-survery_sum, -r_sum) 
+  mutate(survey_percent = survey_counts/survery_sum)  %>% 
+  select(-survery_sum) 
+
+
+## R-ladies data
+### read in the data from google sheets
+
+pie_list <- c("apple", "blueberry", "buttermilk", "cherry", "chocolate", "coconut cream", "key lime", "lemon meringue", "mincemeat", "peach", "pecan", "pumpkin", "sweet potato", "none")
+
+r_ladies_raw <- read_csv(here("data/r_ladies_survey.csv")) 
+
+r_ladies_pies <- r_ladies_raw %>% 
+  dplyr::select("What pies are eaten during your chosen Thanksgiving Day holiday? (Check all that apply)") %>% 
+  rename(pie_selections = "What pies are eaten during your chosen Thanksgiving Day holiday? (Check all that apply)") %>% 
+  mutate(flavor = strsplit(pie_selections, ",")) %>%
+  unnest(flavor)  %>% 
+  mutate(flavor = str_trim(flavor, side = c("both"))) %>% 
+  select(flavor) %>% 
+  count(flavor) %>% 
+  mutate(flavor = ifelse(flavor %!in% pie_list, "other", flavor)) %>% 
+  rename(r_ladies_count = n) %>% 
+  group_by(flavor) %>% 
+  dplyr::summarise(r_ladies_count = sum(r_ladies_count)) %>% 
+  ungroup() %>% 
+  mutate(total = sum(r_ladies_count),
+         r_percent = r_ladies_count/total) %>% 
+  select(-total)
+
+thanksgiving_pie <- left_join(fivethirtyeight_pies, r_ladies_pies, by = "flavor") %>% 
+  mutate(r_ladies_count = ifelse(is.na(r_ladies_count), 0, r_ladies_count),
+         r_percent = ifelse(is.na(r_percent), 0, r_percent)) %>% 
+  dplyr::select(flavor, type, survey_counts, r_ladies_counts = r_ladies_count, survey_percent, r_percent)
 
 write_csv(thanksgiving_pie, here("data/thanksgiving_pie.csv"))
